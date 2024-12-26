8ab11883e9c0625f818ae35586a563955ba17e36
Refaat Hassan <refathassan1289641@gmail.com>
2024-12-26 02:47:24 +0200

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 ls=[35,65,4,756,232,76,67,345,32,56]
-for i in range(1,len(ls)):
-    for j in range(i):
-        if ls[i]<ls[j]:
-            ls[i]+=ls[j]
-            ls[j]=ls[i]-ls[j]
-            ls[i]-=ls[j]
-print(ls)
+def insetion_sort(Arr):
+    for i in range(1,len(Arr)):
+        for j in range(i):
+            if Arr[i]<Arr[j]:
+                Arr[i]+=Arr[j]
+                Arr[j]=Arr[i]-ls[j]
+                Arr[i]-=Arr[j]
+    return Arr
+arr=insetion_sort(ls)
+print(arr)
 
 
 
